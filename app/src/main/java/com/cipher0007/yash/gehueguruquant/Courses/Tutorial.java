3e42182630d05debc7540ef2dbe2ab522a62c395
yash <yash.verma1200@gmail.com>
2018-04-28 13:35:37 +0530

    new update

--- app/src/main/java/com/cipher0007/yash/gehueguruquant/Courses/Tutorial.java
+++ app/src/main/java/com/cipher0007/yash/gehueguruquant/Courses/Tutorial.java
@@ -29,7 +29,7 @@ private CardView unit1,unit2,unit3,unit4,unit5,unit6,unit7;
         unit5=findViewById(R.id.unit5);
         unit6=findViewById(R.id.unit6);
         unit7=findViewById(R.id.unit7);
-
+//asasas
         unit1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
